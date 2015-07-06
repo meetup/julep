4438689ec8e7e140502329b5711be8fb232c259f
Adam Detrick <akdetrick@gmail.com>
2015-07-06 12:28:47 -0400

    stub utils

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.initConfig({
+		bower: grunt.file.readJSON('bower.json'),
+		'sass': {
+			dist: {
+				files: {
+					"scss/yeti.css": "scss/yeti.scss"
+				}
+			}
+		},
+		'clean': {
+		  css: ["scss/*.css", "!scss/*.scss"]
+		}
+	});
+
+	grunt.registerTask('default', ['sass', 'clean']);
+};
